e7aecf9a6d51f524f0ce8ee2b1e2a9cb9c4e5277
DiegoAgudelo4 <diegoagudelo180@gmail.com>
2022-09-04 17:07:36 -0500

    Test unitario link 3

---
+++ TestUnitario3/test/testunitario3/EcuacionPrimerGradoIntegrationTest.java
@@ -0,0 +1,34 @@
+
+package testunitario3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author diego
+ */
+public class EcuacionPrimerGradoIntegrationTest {
+    EcuacionPrimerGrado ecuacion = new EcuacionPrimerGrado();
+
+    @Test
+    public void solucionaEcuacionConMenos() {
+
+        Double result = ecuacion.obtenerResultado("2x - 1 = 0");
+        Double valueExpected = 0.5;
+        assertEquals(valueExpected, result);
+    }
+    @Test
+    public void solucionaEcuacionConMas() {
+        Double result = ecuacion.obtenerResultado("2x + 1 = 0");
+        Double valueExpected = -0.5;
+        assertEquals(valueExpected, result);
+    }
+    @Test
+    public void solucionaEcuacionConParte3Mayor0() {
+        Double result = ecuacion.obtenerResultado("2x + 1 = 10");
+        Double valueExpected = 4.5;
+        assertEquals(valueExpected, result);
+    }
+
+}
