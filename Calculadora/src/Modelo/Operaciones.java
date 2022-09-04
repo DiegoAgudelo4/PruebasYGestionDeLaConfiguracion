dd274609936bea6cea14128116a2d5bf2ab4459a
DiegoAgudelo4 <diegoagudelo180@gmail.com>
2022-09-04 17:02:26 -0500

    test de calculadora

---
+++ Calculadora/src/Modelo/Operaciones.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author diego
+ */
+public class Operaciones {
+    public double suma(double a, double b){
+        return a+b;
+    }
+    public double resta(double a, double b){
+        return a-b;
+    }
+    public double division(double a, double b){
+        return a/b;
+    }
+    public double multiplicacion(double a, double b){
+        return a*b;
+    }
+    public double potenciacion(double a, double b){
+        return (double) Math.pow(a, b);
+    }
+    public double raiz(Double a){
+        return (double) Math.sqrt(a);
+    }
+}
---
+++ Calculadora/test/calculadora/OperacionesTest.java
@@ -0,0 +1,62 @@
+
+package calculadora;
+
+import Modelo.Operaciones;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author diego
+ */
+public class OperacionesTest {
+    private static final double DELTA = 1e-15;
+    private final Operaciones operacion=new Operaciones();
+    private double a, b, resultado;
+    
+    public OperacionesTest() {
+    }
+    
+    @Test
+    public void testsuma() {
+         a= 1;
+         b= 2;
+        resultado = operacion.suma(a,b);
+        assertEquals(3, resultado, DELTA);
+    }
+    @Test
+    public void testResta() {
+         a= 3;
+         b= 2;
+        resultado = operacion.resta(a,b);
+        assertEquals(1, resultado, DELTA);
+    }
+    
+    @Test
+    public void testMultiplicacion() {
+         a= 3;
+         b= 2;
+        resultado = operacion.multiplicacion(a,b);
+        assertEquals(6, resultado, DELTA);
+    }
+    @Test
+    public void testDivision() {
+         a= 20;
+         b= 2;
+        resultado = operacion.division(a,b);
+        assertEquals(10, resultado, DELTA);
+    }
+    @Test
+    public void testPotencia() {
+         a= 3;
+         b= 2;
+        resultado = operacion.potenciacion(a,b);
+        assertEquals(9, resultado, DELTA);
+    }
+    @Test
+    public void testRaiz() {
+         a= 16;
+        resultado = operacion.raiz(a);
+        assertEquals(4, resultado, DELTA);
+    }
+}
